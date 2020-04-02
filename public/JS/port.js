cc06112357dba852f9de436c4c86b95ff51beae4
clueking1 <wcoan300@gmail.com>
2020-04-02 10:11:39 -0400

    added parent div to website images so they overlay

--- public/JS/port.js
+++ public/JS/port.js
@@ -1,8 +1,19 @@
 window.onscroll = function() {myFunction()};
+const itemA = document.querySelector('.a')
+const itemB = document.querySelector('.b')
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    
+    if(window.getComputedStyle(itemA).visibility === "visible") {
+        itemA.style.visibility = 'hidden'
+        itemB.style.visibility = 'visible'
+    }
     window.scrollTo(0, 0)
  
   }
 }
+
+
+
+
