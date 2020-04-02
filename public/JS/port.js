81bf8d746bc6f12a463c6714caacda883a633afa
clueking1 <wcoan300@gmail.com>
2020-04-02 11:10:19 -0400

    changed visibility to opacity and add caption dots

--- public/JS/port.js
+++ public/JS/port.js
@@ -7,30 +7,67 @@ const itemE = document.querySelector('.e')
 const itemF = document.querySelector('.f')
 const itemG = document.querySelector('.g')
 
+const item1 = document.querySelector('.one')
+const item2 = document.querySelector('.two')
+const item3 = document.querySelector('.three')
+const item4 = document.querySelector('.four')
+const item5 = document.querySelector('.five')
+const item6 = document.querySelector('.six')
+const item7 = document.querySelector('.seven')
+
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     
-    if(window.getComputedStyle(itemA).visibility === "visible") {
-        itemA.style.visibility = 'hidden'
-        itemB.style.visibility = 'visible'
-    } else if (window.getComputedStyle(itemB).visibility === "visible") {
-        itemB.style.visibility = 'hidden'
-        itemC.style.visibility = 'visible'
-    } else if (window.getComputedStyle(itemC).visibility === "visible") {
-        itemC.style.visibility = 'hidden'
-        itemD.style.visibility = 'visible'
-    } else if (window.getComputedStyle(itemD).visibility === "visible") {
-        itemD.style.visibility = 'hidden'
-        itemE.style.visibility = 'visible'
-    } else if (window.getComputedStyle(itemE).visibility === "visible") {
-        itemE.style.visibility = 'hidden'
-        itemF.style.visibility = 'visible'
-    } else if (window.getComputedStyle(itemF).visibility === "visible") {
-        itemF.style.visibility = 'hidden'
-        itemG.style.visibility = 'visible'
+    if(window.getComputedStyle(itemA).opacity === "1") {
+        itemA.style.opacity = '0'
+        itemB.style.opacity = '1'
+
+        item1.style.backgroundColor = '#bbb'
+        item2.style.backgroundColor = '#474747'
+
+    } else if (window.getComputedStyle(itemB).opacity=== "1") {
+        itemB.style.opacity= '0'
+        itemC.style.opacity= '1'
+
+        item2.style.backgroundColor = '#bbb'
+        item3.style.backgroundColor = '#474747'
+
+    } else if (window.getComputedStyle(itemC).opacity=== "1") {
+        itemC.style.opacity= '0'
+        itemD.style.opacity= '1'
+
+
+        item3.style.backgroundColor = '#bbb'
+        item4.style.backgroundColor = '#474747'        
+
+    } else if (window.getComputedStyle(itemD).opacity=== "1") {
+        itemD.style.opacity= '0'
+        itemE.style.opacity= '1'
+
+        item4.style.backgroundColor = '#bbb'
+        item5.style.backgroundColor = '#474747'
+
+    } else if (window.getComputedStyle(itemE).opacity=== "1") {
+        itemE.style.opacity= '0'
+        itemF.style.opacity= '1'
+
+        item5.style.backgroundColor = '#bbb'
+        item6.style.backgroundColor = '#474747'
+
+    } else if (window.getComputedStyle(itemF).opacity=== "1") {
+        itemF.style.opacity= '0'
+        itemG.style.opacity= '1'
+
+        item6.style.backgroundColor = '#bbb'
+        item7.style.backgroundColor = '#474747'
+
     } else {
-        itemG.style.visibility = 'hidden'
-        itemA.style.visibility = 'visible'
+        itemG.style.opacity= '0'
+        itemA.style.opacity= '1'
+
+        item7.style.backgroundColor = '#bbb'
+        item1.style.backgroundColor = '#474747'
+
     }
     window.scrollTo(0, 0)
  
